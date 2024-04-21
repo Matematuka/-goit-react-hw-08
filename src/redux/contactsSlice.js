3c5c7e4c07a74dddedf67e6d5798c3cc902c65d0
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-04-21 21:07:16 +0300

    reducers

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -34,7 +34,7 @@ const contactsSlice = createSlice({
       })
       .addCase(addContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.items = action.payload;
+        state.items.push(action.payload);
       })
       .addCase(addContact.rejected, (state) => {
         state.loading = false;
@@ -47,7 +47,9 @@ const contactsSlice = createSlice({
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.items = action.payload;
+        state.items = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
       })
       .addCase(deleteContact.rejected, (state) => {
         state.loading = false;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice.js";
 import { filtersReduser } from "./filtersSlice.js";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-const phonebookPeristConfig = {
-  key: "phonebook",
-  storage,
-  whitelist: ["items"],
-};
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(phonebookPeristConfig, contactsReducer),
+    contacts: contactsReducer,
     filters: filtersReduser,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
